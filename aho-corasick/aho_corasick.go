0371c8920817cea9375b064fbc7dd9984f693124
Claude Betz <claude.betz47@gmail.com>
2022-11-06 17:28:08 +0200

    added building of goto function

---
+++ aho-corasick/aho_corasick.go
@@ -0,0 +1,97 @@
+package main
+
+import(
+	"fmt"
+)
+
+var (
+	id int = -1
+)
+
+type State struct {
+	id int
+	states []*State
+	accepts rune
+}
+
+func NewState(accepts rune) *State {
+	stateArr := make([]*State, 0)
+	id++
+
+	return &State{
+		id: id,
+		states: stateArr,
+		accepts: accepts,
+	}
+
+}
+
+func (s *State) InsertKeyword(kw []rune) {
+	currState := s
+
+	// iterate over runes in keyword
+	for _, r := range kw {
+		foundValidState := false
+
+		// iterate over states
+		for _, state := range currState.states {
+			// try insert substring into valid state
+			if state.isValidTransition(r) {
+				currState = state	
+				foundValidState = true
+				fmt.Printf("%s\n", state.ToString())		
+				break
+			}
+		}
+		// no valid transition for c make new state and insert
+		if (foundValidState == false) {
+			// create new state and append to currStates list of valid states
+			newState := NewState(r)
+			currState.states = append(currState.states, newState)
+			currState = newState
+		}
+	}
+}
+
+func (s *State) GetKeywordStates(kw []rune) []int {
+	currState := s
+	arr := []int{0} 
+	for _, r := range kw {
+		if currState.states != nil { 
+			for _, state := range currState.states {
+				if state.isValidTransition(r) {
+					fmt.Printf("%s\n", state.ToString())
+					arr = append(arr, state.id)
+					currState = state
+					break
+				} else {
+					fmt.Println("no valid transition")
+				}
+			} 
+		}
+	}	
+	return arr
+}
+
+
+func (s *State) isValidTransition(r rune) bool {
+	return s.accepts == r
+}
+
+func (s *State) ToString() string {
+	return fmt.Sprintf("id:%d, accepts:%s", s.id, string(s.accepts))
+}
+
+func main() {
+	fmt.Println("Aho Corasick Algorithm")
+	
+	root := NewState(-1)
+
+	root.InsertKeyword([]rune("car"))
+	root.InsertKeyword([]rune("tim"))
+	root.InsertKeyword([]rune("cards"))
+
+	fmt.Printf("transitions: %v\n", root.GetKeywordStates([]rune("car")))
+	fmt.Printf("transitions: %v\n", root.GetKeywordStates([]rune("tim")))
+	fmt.Printf("transitions: %v\n", root.GetKeywordStates([]rune("cards")))
+}
---
+++ aho-corasick/aho_corasick_test.go
@@ -0,0 +1,59 @@
+package main
+
+import(
+	"testing"
+)
+
+var tests = []struct {
+	patterns []string
+	output [][]int
+}{
+	{
+		[]string{
+			"car",
+			"dog",
+		},
+		[][]int{
+			{0,1,2,3},
+			{0,4,5,6},
+		},
+	},
+}
+
+func TestInsertAndRetrieveWord(t *testing.T) {
+	
+	for _, test := range tests {
+
+		// Create root node
+		root := NewState(-1) 
+		
+		for i:=0; i<len(test.patterns); i++ {
+			// insert
+			root.InsertKeyword([]rune(test.patterns[i]))
+
+			// get transitions
+			validStates := root.GetKeywordStates([]rune(test.patterns[i]))
+
+			// validate
+			eq := checkEquality(validStates, test.output[i])
+			if eq == false {
+				t.Errorf("expected: %v, got: %v\n", []int(test.output[i]), validStates)
+			}
+		}
+	} 
+} 
+
+func checkEquality(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i:=0; i<len(a); i++ {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
