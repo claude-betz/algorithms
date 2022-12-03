bfabef7e1ee2019ea269edcd15241fc968c9fab2
Claude Betz <claude.betz47@gmail.com>
2022-12-03 21:02:43 +0200

    moved things around, added simple test for regex to nfa conversion

---
+++ automata/regex-to-nfa/regex_to_nfa_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"testing"
+)
+
+var tests = []struct {
+	alphabet []rune
+	regex string
+	input []string
+	output []bool
+}{
+	{
+		[]rune{'a', 'b'},
+		"a|b",
+		[]string{"a", "b", "c"},
+		[]bool{true, true, false},
+	},
+}
+
+func TestRegexToNFA(t *testing.T) {
+	
+}
