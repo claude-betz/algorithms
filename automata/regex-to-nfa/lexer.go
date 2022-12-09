a886c15e40f038df8fbfa5a1cd3e6b56196d8648
Claude Betz <claude.betz47@gmail.com>
2022-12-09 09:24:47 +0200

    implemented and tested basic lexing for regex

---
+++ automata/regex-to-nfa/lexer.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"errors"
+	"unicode"
+)
+
+var (
+	tagError = errors.New("can not tag") 
+	errEOF = errors.New("end of file")
+)
+
+type tagval struct {
+	Tag Tag
+	Value string
+}
+
+type Lexer struct {
+	input []rune
+	pos int
+}
+
+func NewLexer(input string) *Lexer {
+	return &Lexer{input: []rune(input)}
+}
+
+func (l *Lexer) ReadToken() (*Token, error) {
+	n, err := skipWhitespace(l.input[l.pos:])
+	if err != nil {
+		if err == errEOF {
+			return eof, nil
+		}
+		return nil, err 
+	}
+	l.pos += n
+
+	taggers := []func(input []rune) (*tagval, error) {
+		readPunctuation,
+		readId,
+	}
+	
+	for _, tagger := range taggers {
+		tv, err := tagger(l.input[l.pos:])
+		if err == nil {
+			tk := &Token{l.pos, tv.Tag, tv.Value}
+			l.pos++
+			return tk, nil
+		}
+		if err != tagError {
+			panic(fmt.Sprintf("unknown error: %s", err))
+		}
+	}
+	return nil, fmt.Errorf("cannot recognise rune: %c", l.input[l.pos])
+}
+
+func skipWhitespace(input []rune) (int, error) {
+	for n := 0; n < len(input); n++ {
+		if !unicode.IsSpace(input[n]) {
+			return n, nil
+		}
+	}
+	return 0, errEOF
+}
+
+func readPunctuation(input []rune) (*tagval, error) {
+	r := input[0]
+	if isPunctionSymbol[r] {
+		return &tagval{TagPunct, fmt.Sprintf("%c", r)}, nil
+	}
+	return nil, tagError
+} 
+
+func readId(input []rune) (*tagval, error) {
+	r := input[0]
+	if !unicode.IsLetter(input[0]) {
+		return nil, tagError
+	}
+	return &tagval{TagId, string(r)}, nil	
+}
+
---
+++ automata/regex-to-nfa/lexer_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"testing"
+)
+
+var tests = []struct {
+	regex []rune
+	tokens []*Token
+}{
+	{
+		[]rune("a|b"),
+		[]*Token{
+			&Token{Tag: TagId, Value: "a"},
+			&Token{Tag: TagPunct, Value: "|"},
+			&Token{Tag: TagId, Value: "b"},
+		},
+	},
+}
+
+func testLexing(t *testing.T) {
+	for _, test := range tests {
+		lex := &Lexer{input: test.regex}
+
+		var lexedTokens []*Token
+		for tk, _ := lex.ReadToken(); tk.Value != EOF; tk, _ = lex.ReadToken() {
+			lexedTokens = append(lexedTokens, tk)	
+		}
+
+		// validate
+		for i, expectedToken := range test.tokens {
+			if !areEqual(expectedToken, lexedTokens[i]) {
+				t.Errorf("expected: %v but got: %v", expectedToken, lexedTokens[i])		
+			}		
+		}
+	}
+}
+
+func areEqual(a, b *Token) bool {
+	if a.Tag == b.Tag && a.Value == b.Value {
+		return true
+	} 
+	return false
+}
+
