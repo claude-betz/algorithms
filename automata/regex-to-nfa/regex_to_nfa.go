100818624b763324861d5cd5adb52f083091fca9
Claude Betz <claude.betz47@gmail.com>
2023-01-04 15:16:30 +0200

    initial driver

--- automata/regex-to-nfa/regex_to_nfa.go
+++ automata/regex-to-nfa/regex_to_nfa.go
@@ -1,2 +1,25 @@
 package main
 
+import (
+	"fmt"
+)
+
+var (
+	program = []rune("(a|b)*abb")
+)
+
+func main() {
+	fmt.Println(string(program))
+	lex := &Lexer{input: program}
+
+	var lexedTokens []*Token
+	for tk, _ := lex.ReadToken(); tk.Value != EOF; tk, _ = lex.ReadToken() {
+		lexedTokens = append(lexedTokens, tk)
+	}
+
+	fmt.Printf("%v\n", lexedTokens)
+
+	union(lex)	
+}
+
+
