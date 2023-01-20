b9eb56846e29fc7bf4144b9898093edb82008c50
Claude Betz <claude.betz47@gmail.com>
2023-01-20 14:59:25 +0200

    fix representation to allow for multiple transitions on same char

--- automata/regex-to-nfa/nfa.go
+++ automata/regex-to-nfa/nfa.go
@@ -4,25 +4,17 @@ import (
 	"fmt"
 )
 
+const (
+	eps = 'ε'
+)
+
 type nfa struct {
 	accepting bool
-	edges map[rune]*nfa
+	edges map[rune][]*nfa
 }
 
-func (n *nfa) AddState(char rune, accepting bool) *nfa {
-	nfa := &nfa{
-		accepting: accepting,
-		edges: make(map[rune]*nfa),
-	}
 
-	// add state transition
-	n.edges[char] = nfa
-
-	// return new state
-	return nfa
-}
-
-func (n *nfa) Print() {
+func (n *nfa) PrintNFA() {
 	// need to track assigned state numbers
 	var seen = make(map[*nfa]int)
 	
@@ -33,18 +25,20 @@ func (n *nfa) Print() {
 	var stateId = 0
 
 	// populate level 0  
-	for key, nextState := range n.edges {	
-		// increment stateId
-		stateId++
+	for key, nextStates := range n.edges {	
+		for _, nextState := range nextStates {
+			// increment stateId
+			stateId++
 
-		// print state
-		fmt.Printf("[%d]-%s->[%d]\n", 0, string(key), stateId)
+			// print state
+			fmt.Printf("[%d]-%s->[%d]\n", 0, string(key), stateId)
 
-		// add to seen map
-		seen[nextState] = stateId
+			// add to seen map
+			seen[nextState] = stateId
 
-		// add to queue
-		queue = append(queue, nextState)
+			// add to queue
+			queue = append(queue, nextState)
+		}
 	}
 
 	for {
@@ -59,44 +53,66 @@ func (n *nfa) Print() {
 		// currStateId
 		currStateId := seen[curr]
 
-		for char, nextState := range curr.edges {			
-			val, ok := seen[nextState]
+		for char, nextStates := range curr.edges {			
+			for _, nextState := range nextStates {
+				val, ok := seen[nextState]
 
-			// we have not seen this state before
-			if !ok {
-				// increment state
-				stateId++
+				// we have not seen this state before
+				if !ok {
+					// increment state
+					stateId++
 
-				// add to val
-				val = stateId
+					// add to val
+					val = stateId
 
-				// add to seen
-				seen[nextState] = stateId	
+					// add to seen
+					seen[nextState] = stateId	
 
-				// add to queue
-				queue = append(queue, nextState)
+					// add to queue
+					queue = append(queue, nextState)
+				}
+
+				// print
+				fmt.Printf("[%d]-%s->[%d]\n", currStateId, string(char), val)
 			}
-
-			// print
-			fmt.Printf("[%d]-%s->[%d]\n", currStateId, string(char), val)			
 		}		 
 	}
 }
 
+
 func main() {
-	n := nfa{
-		accepting: false,
-		edges: make(map[rune]*nfa),
+	end := []*nfa{
+		&nfa{
+			false,
+			map[rune][]*nfa{},
+		},
+	}
+	
+	nfa1 := &nfa{
+		false,
+		map[rune][]*nfa{
+			'a': end,
+			'b': end,
+		},
+	}
+	
+	nfa2 := &nfa{
+		false,
+		map[rune][]*nfa{
+			'c': end,
+		},
 	}
 
-	a := n.AddState('a', false)
-	n.AddState('b', false)
+	start := &nfa{
+		false,
+		map[rune][]*nfa{
+			eps: []*nfa{
+				nfa1,
+				nfa2,
+			},
+		},
+	}	
 
-	c := a.AddState('c', false)
-	d := c.AddState('d', false)
-	
-	d.AddState('e', true)
-
-
-	n.Print()
+	start.PrintNFA()
 }
+
